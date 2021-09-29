685966a40598ac5e9a3a5c00811a505cd6ac60c2
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-29 22:16:12 +0530

    added step 1 of cab invoice

---
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -0,0 +1,13 @@
+package com.cabinvoice;
+
+public class CabInvoiceGenerator {
+
+	private final int MINIMUM_FARE = 5;
+
+	public double generateFare(double distance, double time) {
+		double cost = (distance * 10) + (1 * time);
+		return cost > MINIMUM_FARE ? cost : MINIMUM_FARE;
+	}
+
+
+}
